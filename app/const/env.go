1539d38d2fdf95476a4ab4e25621fd44e8a033da
dsrice <v.fuji.0223@gmail.com>
2023-12-17 22:59:34 +0900

    add: DB接続周りを整備

---
+++ app/const/env.go
@@ -0,0 +1,6 @@
+package _const
+
+const (
+	// EnvDbName DB名称(ENV)
+	EnvDbName = "DB_NAME"
+)
---
+++ app/infra/database/connection/conn.go
@@ -0,0 +1,38 @@
+package connection
+
+import (
+	"database/sql"
+	"github.com/go-sql-driver/mysql"
+	"time"
+)
+
+type Connection struct {
+	Conn *sql.DB
+}
+
+func (c *Connection) GetConnection() error {
+	jst, err := time.LoadLocation("Asia/Tokyo")
+	if err != nil {
+		return err
+	}
+
+	conf := mysql.Config{
+		DBName:    "bowling_score",
+		User:      "user",
+		Passwd:    "password",
+		Addr:      "db:3306",
+		Net:       "tcp",
+		Collation: "utf8mb4_unicode_ci",
+		Loc:       jst,
+	}
+
+	db, err := sql.Open("mysql", conf.FormatDSN())
+
+	if err != nil {
+		return err
+	} else {
+		c.Conn = db
+	}
+
+	return nil
+}
